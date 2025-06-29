8004a5418a130ad36321d0e5a00e77bfc06012c2
natour <bgd4500@gmail.com>
2025-06-29 18:49:14 +0300

    Feature: add constants object

---
+++ app/src/main/java/natour/dev/zonetechnologiestask/core/util/Contstants.kt
@@ -0,0 +1,9 @@
+package natour.dev.zonetechnologiestask.core.util
+
+object Constants {
+    const val BASE_URL =  "http://192.168.1.9:8082/"
+    const val DEVICE_ENDPOINT = "api/devices"
+    const val PUSH_LOCATION_PORT = 5055
+    const val DEVICE_ID_KEY = "deviceId" // declared here because appstrings cannot be accessed in viewmodel
+
+}

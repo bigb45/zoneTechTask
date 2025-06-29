a6553ac59f661731be96cd95ee5ae9da73dffeef
natour <bgd4500@gmail.com>
2025-06-29 18:47:51 +0300

    Feature: register device on first launch

--- app/src/main/java/natour/dev/zonetechnologiestask/ui/MainActivity.kt
+++ app/src/main/java/natour/dev/zonetechnologiestask/ui/MainActivity.kt
@@ -1,29 +1,40 @@
-package natour.dev.zonetechnologiestask
+package natour.dev.zonetechnologiestask.ui
 
-import android.Manifest.permission.ACCESS_COARSE_LOCATION
-import android.Manifest.permission.ACCESS_FINE_LOCATION
-import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import natour.dev.zonetechnologiestask.R
+import natour.dev.zonetechnologiestask.core.util.SharedPreferencesUtil
+import natour.dev.zonetechnologiestask.core.util.UniqueIdUtil.generateRandomId
 import natour.dev.zonetechnologiestask.databinding.ActivityMainBinding
+import natour.dev.zonetechnologiestask.ui.firstFragment.FirstFragmentViewmodel
+import natour.dev.zonetechnologiestask.ui.services.LocationTrackingService
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private val viewmodel = FirstFragmentViewmodel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        SharedPreferencesUtil.init(this)
+        var deviceId = SharedPreferencesUtil.getValue(
+            getString(R.string.deviceId), default = ""
+        )
+        if (deviceId.isEmpty()) {
+            deviceId = generateRandomId()
+            SharedPreferencesUtil.setValue(
+                getString(R.string.deviceId),
+                deviceId
+            )
+            viewmodel.registerNewDevice(deviceId)
+        }
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -35,20 +46,16 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
+
     }
 
     fun startTrackingService() {
         val foregroundServiceIntent = Intent(this, LocationTrackingService::class.java)
         this.startForegroundService(foregroundServiceIntent)
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }

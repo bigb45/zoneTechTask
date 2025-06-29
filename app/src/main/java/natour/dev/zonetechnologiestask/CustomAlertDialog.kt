087e3143ef4e79e86a5deca0a40d5bb30ec02615
natour <bgd4500@gmail.com>
2025-06-29 18:49:35 +0300

    Refactor: make dialog customizable

--- app/src/main/java/natour/dev/zonetechnologiestask/CustomAlertDialog.kt
+++ app/src/main/java/natour/dev/zonetechnologiestask/CustomAlertDialog.kt
@@ -2,34 +2,35 @@ package natour.dev.zonetechnologiestask
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.Intent
 import android.os.Bundle
-import android.provider.Settings
 import androidx.fragment.app.DialogFragment
-import androidx.core.net.toUri
 
-class CustomAlertDialog: DialogFragment() {
+class CustomAlertDialog : DialogFragment() {
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return activity?.let {
-            // Use the Builder class for convenient dialog construction.
-            val builder = AlertDialog.Builder(it)
-            builder.setMessage("Start game")
-                .setCancelable(false)
-                .setPositiveButton("Start") { dialog, id ->
-                    val settingsIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).also {
-                        val uri = "package:${context?.packageName}".toUri()
-                        it.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                        it.data = uri
-
-                    }
-                    startActivity(settingsIntent)
-                }
-
-
-            builder.create()
-        } ?: throw IllegalStateException("Activity cannot be null")
+    companion object {
+        fun newInstance(
+            message: String,
+            positiveText: String,
+            onPositive: () -> Unit,
+            cancelable: Boolean = true
+        ): CustomAlertDialog {
+            return CustomAlertDialog().apply {
+                this.message = message
+                this.positiveText = positiveText
+                this.onPositive = onPositive
+                this.isCancelable = cancelable
+            }
+        }
     }
 
+    private lateinit var message: String
+    private lateinit var positiveText: String
+    private var onPositive: (() -> Unit)? = null
 
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return requireActivity().let {
+            AlertDialog.Builder(it).setMessage(message).setCancelable(isCancelable)
+                .setPositiveButton(positiveText) { _, _ -> onPositive?.invoke() }.create()
+        }
+    }
 }
